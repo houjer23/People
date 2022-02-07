2d0bea4284f316945fc662e789db7dab4c9e2f81
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-02-07 09:36:22 -0500

    Create Scholar.java

---
+++ Scholar.java
@@ -0,0 +1,12 @@
+public class Scholar extends Person {
+	private String major;
+	
+	public Scholar(String name, int birth, String major) {
+		super(name, birth);
+		this.major = major;
+	}
+	
+	public String toString() {
+		return super.toString() + " Major: " + major;
+	}
+}
